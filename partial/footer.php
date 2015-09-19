ecd514f389328c5f8123bb25f81398edc81a97fb
Wagner Santos <wagneersantos@gmail.com>
2015-09-19 12:07:58 -0300

    construindo header responsivo

--- partial/footer.php
+++ partial/footer.php
@@ -1,1 +1,24 @@
-footer
+</section>
+		<script src="assets/js/jquery-2.1.4.min.js"></script>
+		<!--<script src="assets/js/jquery.validate.min.js"></script>
+		<script src="assets/js/validate.js"></script>
+		<script src="assets/js/mask.js"></script> -->
+		
+		
+		<script>
+		$(document).ready(function() {
+			//$('.phone').mask('00000-0000');
+			$(".menu-drop").click(function(){
+				$(".box-menu").slideToggle();
+				//$(".box-menu").slideToggle(function(){
+					//if ($(".box-menu").is(':visible')) {
+		                //$(".menu-drop span").text("X");
+		            //} else {
+		                 //$(".menu-drop span").text("");
+		            //}
+				//});
+			});  			
+		});
+		</script>
+	</body>
+</html>
--- partial/header.php
+++ partial/header.php
@@ -1,1 +1,14 @@
-header
+<!DOCTYPE HTML>
+<html lang="pt-BR">
+	<head>
+		<title>Wagner Santos Front-end Developer</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1">
+		<link rel="shortcut icon" href="assets/images/favicon.ico">
+		<link rel="stylesheet" type="text/css" href="assets/css/reset.css" media="all" />
+		<link rel="stylesheet" type="text/css" href="assets/css/master.css" media="all" />
+		<link rel="stylesheet" type="text/css" href="assets/css/home.css" media="all" />
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<section class="site">
