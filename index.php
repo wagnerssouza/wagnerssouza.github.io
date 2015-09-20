3f8463402392b0c07ac2b554a8e09d0851ff32be
Wagner Santos <wagneersantos@gmail.com>
2015-09-20 01:22:32 -0300

    ajustando espacamento na area de ultimos trabalhos

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <div class="box-full about">
 	<div class="conteudo content-center">
 		<div class="content-about">
-			<h1>Hi, I am Wagner</h1>
+			
 			<img src="http://gentsthemes.com/demo/stanleywp/wp-content/uploads/sites/5/2014/01/user.png" alt="" class="img-about">
 			<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de.</p>
 			<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de.</p>	
@@ -16,49 +16,49 @@
 <div class="box-full works">
 
 	<div class="conteudo content-center">
-		<div class="col-desk-3 col-tablet-6" style="background: red">
+		<div class="item-work margin-work">
 			<a href="#" class="hover-works">
 				<span></span>
 				<img src="assets/images/trabalho.jpg" alt="">
 			</a>
 		</div>
-		<div class="col-desk-3 col-tablet-6" style="background: red">
+		<div class="item-work margin-work margin-work-mobile">
 			<a href="#" class="hover-works">
 				<span></span>
 				<img src="assets/images/trabalho.jpg" alt="">
 			</a>
 		</div>
-		<div class="col-desk-3 col-tablet-6" style="background: red">
+		<div class="item-work margin-work">
 			<a href="#" class="hover-works">
 				<span></span>
 				<img src="assets/images/trabalho.jpg" alt="">
 			</a>
 		</div>
-		<div class="col-desk-3 col-tablet-6" style="background: red">
+		<div class="item-work">
 			<a href="#" class="hover-works">
 				<span></span>
 				<img src="assets/images/trabalho.jpg" alt="">
 			</a>
 		</div>
-		<div class="col-desk-3 col-tablet-6" style="background: red">
+		<div class="item-work margin-work">
 			<a href="#" class="hover-works">
 				<span></span>
 				<img src="assets/images/trabalho.jpg" alt="">
 			</a>
 		</div>
-		<div class="col-desk-3 col-tablet-6" style="background: red">
+		<div class="item-work margin-work margin-work-mobile">
 			<a href="#" class="hover-works">
 				<span></span>
 				<img src="assets/images/trabalho.jpg" alt="">
 			</a>
 		</div>
-		<div class="col-desk-3 col-tablet-6" style="background: red">
+		<div class="item-work margin-work">
 			<a href="#" class="hover-works">
 				<span></span>
 				<img src="assets/images/trabalho.jpg" alt="">
 			</a>
 		</div>
-		<div class="col-desk-3 col-tablet-6" style="background: red">
+		<div class="item-work">
 			<a href="#" class="hover-works">
 				<span></span>
 				<img src="assets/images/trabalho.jpg" alt="">
